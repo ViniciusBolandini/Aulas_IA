ee514757d722431d3da95ca2f5781a0d28327a4e
Vinicius Bolandini <125499098+ViniciusBolandini@users.noreply.github.com>
2025-09-18 16:13:33 -0300

    Update graphs.py para grafo Romenia

--- heuristica/graphs.py
+++ heuristica/graphs.py
@@ -1,8 +1,23 @@
-graph_ag = {
-    'A': {'B': 12, 'C': 14},
-    'B': {'C': 9, 'D': 38},
-    'C': {'D': 24, 'E': 7},
-    'D': {'G': 9},
-    'E': {'D': 13, 'F': 9, 'G': 29},
-    'F': {}
+romania_graph = {
+    'Arad': {'Zerind': 75, 'Sibiu': 140, 'Timisoara': 118},
+    'Bucharest': {'Urziceni': 85, 'Pitesti': 101, 'Giurgiu': 90, 'Fagaras': 211},
+    'Craiova': {'Dobreta': 120, 'Rimnicu Vilcea': 146, 'Pitesti': 138},
+    'Dobreta': {'Mehadia': 75, 'Craiova': 120},
+    'Eforie': {'Hirsova': 86},
+    'Fagaras': {'Sibiu': 99, 'Bucharest': 211},
+    'Giurgiu': {'Bucharest': 90},
+    'Hirsova': {'Urziceni': 98, 'Eforie': 86},
+    'Iasi': {'Vaslui': 92, 'Neamt': 87},
+    'Lugoj': {'Timisoara': 111, 'Mehadia': 70},
+    'Mehadia': {'Lugoj': 70, 'Dobreta': 75},
+    'Neamt': {'Iasi': 87},
+    'Oradea': {'Zerind': 71, 'Sibiu': 151},
+    'Pitesti': {'Rimnicu Vilcea': 97, 'Bucharest': 101, 'Craiova': 138},
+    'Rimnicu Vilcea': {'Sibiu': 80, 'Pitesti': 97, 'Craiova': 146},
+    'Sibiu': {'Arad': 140, 'Oradea': 151, 'Fagaras': 99, 'Rimnicu Vilcea': 80},
+    'Timisoara': {'Arad': 118, 'Lugoj': 111},
+    'Urziceni': {'Bucharest': 85, 'Hirsova': 98, 'Vaslui': 142},
+    'Vaslui': {'Iasi': 92, 'Urziceni': 142},
+    'Zerind': {'Arad': 75, 'Oradea': 71}
 }
+
